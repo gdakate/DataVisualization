317faffa6751c5d77dec50985bec2da61373f3f9
gdakate <gdakate1215@gmail.com>
2024-08-05 17:01:42 +0900

    [feat] 255단위로 갱신 ver.
    필요
    - 업데이트 시 기존 데이터 제거되며 커서가 가운데에 있는것 처럼 보이게 하기
    - 클래스 분리
    - ui만지기

--- npgraph2.py
+++ npgraph2.py
@@ -1,11 +1,14 @@
+import re
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QComboBox
 from PyQt5.QtChart import QChart, QLineSeries, QChartView, QValueAxis, QScatterSeries
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt, QTimer
 import threading
 import serial
 import queue
+import serial.tools.list_ports as sp
+
 
 
 class Main(QMainWindow):
@@ -16,7 +19,7 @@ class Main(QMainWindow):
         self.x = 0
         self.max_points = 255
         self.setWindowTitle("Real-time Graph")
-        self.setGeometry(100, 100, 1200, 800)  # 전체 윈도우 크기 설정
+        self.setGeometry(100, 100, 700, 500)  # 전체 윈도우 크기 설정
 
         self.series1 = QLineSeries()
         self.series2 = QLineSeries()
@@ -25,19 +28,19 @@ class Main(QMainWindow):
         self.cursor_series1 = QScatterSeries()
 
         pen1 = QPen(Qt.magenta)
-        pen1.setWidth(3)
+        pen1.setWidth(2)
         self.series1.setPen(pen1)
 
         pen2 = QPen(Qt.blue)
-        pen2.setWidth(3)
+        pen2.setWidth(2)
         self.series2.setPen(pen2)
 
         pen3 = QPen(Qt.darkMagenta)
-        pen3.setWidth(3)
+        pen3.setWidth(2)
         self.series3.setPen(pen3)
 
         pen4 = QPen(Qt.darkGreen)
-        pen4.setWidth(3)
+        pen4.setWidth(2)
         self.series4.setPen(pen4)
 
         self.cursor_series1.setColor(Qt.red)
@@ -107,7 +110,16 @@ class Main(QMainWindow):
         self.chart_view3.setRenderHint(QPainter.Antialiasing)
         self.chart_view4.setRenderHint(QPainter.Antialiasing)
 
+        self.cb = QComboBox(self)
+        self.numberlist = self.identify_port()
+        for i in self.numberlist:
+            self.cb.addItem("COM " + str(i))
+        self.cb.setMinimumSize(10, 25)
+        self.combox_select()
+
         self.central_widget = QWidget()
+        self.menu_widget = QWidget()
+        self.setMenuWidget(self.menu_widget)
         self.setCentralWidget(self.central_widget)
 
         self.start_button = QPushButton("START")
@@ -116,25 +128,37 @@ class Main(QMainWindow):
         self.stop_button = QPushButton("STOP")
         self.stop_button.clicked.connect(self.stop)
 
-        # h_layout = QHBoxLayout(self.central_widget)
-        # h_layout.addWidget(self.start_button)
-        # h_layout.addWidget(self.stop_button)
+        self.close_button = QPushButton("CLOSE")
+        self.close_button.clicked.connect(self.close)
+
+        h_layout = QHBoxLayout(self.menu_widget)
+        h_layout.addWidget(self.cb)
+        h_layout.addWidget(self.start_button)
+        h_layout.addWidget(self.stop_button)
+        h_layout.addWidget(self.close_button)
 
         v_layout = QVBoxLayout(self.central_widget)
-        # v_layout.addLayout(h_layout)
+        v_layout.addLayout(h_layout)
         v_layout.addWidget(self.chart_view)
         v_layout.addWidget(self.chart_view2)
         v_layout.addWidget(self.chart_view3)
         v_layout.addWidget(self.chart_view4)
-
 
         # Timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_chart)
         self.timer.start(1)
 
+        for chart_view in [self.chart_view, self.chart_view2, self.chart_view3, self.chart_view4]:
+            chart_view.chart().axes(Qt.Horizontal)[0].setVisible(False)
+            chart_view.chart().axes(Qt.Vertical)[0].setVisible(False)
+            chart_view.chart().setBackgroundRoundness(0)
+            chart_view.chart().setBackgroundVisible(False)
+            for axis in chart_view.chart().axes():
+                axis.setGridLineVisible(False)  # 그리드 숨김
+
     def read_serial_sensor(self):
-        self.ser = serial.Serial(port='COM6', baudrate=9600)
+        self.ser = serial.Serial(port='COM'+self.combox_select(), baudrate=9600)
         try:
             while True:
                 serialData = self.ser.readline().decode().rstrip()
@@ -168,16 +192,42 @@ class Main(QMainWindow):
             self.axis_x3.setRange(left_range, right_range)
             self.axis_x4.setRange(left_range, right_range)
 
+    def run(stop_event):
+        while True:
+            if stop_event.is_set():
+                break
     def start(self):
+        # self.run()
         self.serialSensorReadThread = threading.Thread(target=self.read_serial_sensor, daemon=True)
         self.serialSensorReadThread.start()
 
     def stop(self):
         self.timer.stop()
+        # self.serialSensorReadThread.close()
+        # self.serialSensorReadThread.setDaemon(True)
+    def close(self):
+        self.close()
+    def identify_port(self):
+        self.ports = sp.comports()
+        self.nlist = []
+        for port in self.ports:
+            self.number = re.sub(r'[^0-9]', '', port.device)
+            self.nlist.append(self.number)
+        return self.nlist
+
+    def combox_select(self):
+        self.cur_number = re.sub(r'[^0-9]', '', self.cb.currentText())
+        return self.cur_number
+
+    def restart(self):
+        self.serialSensorReadThread = threading.Thread(target=self.read_serial_sensor, daemon=True)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Main()
+
     window.show()
-    window.start()
+    # window.start()
     sys.exit(app.exec_())
